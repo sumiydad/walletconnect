5a1f0bd1628bcd13bffcd92a19373d30be27e1b1
sumiydad <61118506+sumiydad@users.noreply.github.com>
2025-05-22 22:13:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-"scripts": {
-  "start": "node server.js"
+{
+  "scripts": {
+    "start": "node index.js"  // or "node server.js" if you keep that filename
+  },
+  "dependencies": {
+    "@walletconnect/node": "^1.0.0",
+    "express": "^4.17.1",
+    "qrcode": "^1.5.0",
+    "tronweb": "^4.1.0",
+    "cors": "^2.8.5"
+  }
 }
